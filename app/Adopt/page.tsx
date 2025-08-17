b50a285aac4d0994af95b7fab65a231f0ffb3f05
Narayan Naik <125474254+Narayan-cpu@users.noreply.github.com>
2025-08-17 12:38:33 +0530

    Update page.tsx

--- app/Adopt/page.tsx
+++ app/Adopt/page.tsx
@@ -1,20 +1,23 @@
+import { db } from "@/db/client";
+import { pets } from "@/db/schema";
+
 export default async function AdoptPage() {
   const allPets = await db.select().from(pets);
-  console.log("ALL PETS:", allPets); // check server logs
-
-  if (allPets.length === 0) {
-    return <p className="p-8 text-center text-red-500">No pets found in DB</p>;
-  }
 
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold mb-8 text-center">Adopt a Pet</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {allPets.map((pet) => (
-          <div key={pet.id} className="p-6 shadow-lg rounded-lg">
-            <h2 className="text-xl font-bold">{pet.name}</h2>
-            <p className="text-gray-600">{pet.type}</p>
-            <p className="mt-2 text-sm text-gray-700">{pet.description}</p>
+          <div
+            key={pet.id}
+            className="p-6 shadow-lg rounded-lg hover:shadow-[0_0_20px_5px_rgba(128,0,255,0.5)] transition-all duration-300 transform hover:scale-105"
+          >
+            <div className="mt-4">
+              <h2 className="text-xl font-bold">{pet.name}</h2>
+              <p className="text-gray-600">{pet.type}</p>
+              <p className="mt-2 text-sm text-gray-700">{pet.description}</p>
+            </div>
           </div>
         ))}
       </div>
